071ac06cce57ecc94b7aedbdbdb02f43cc4b43ae
Marcin Kass <marcin.kass10@gmail.com>
2018-11-30 18:44:18 +0100

    validation form for training

--- portfolio/Controllers/HomeController.cs
+++ portfolio/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using portfolio.Models;
 
 namespace portfolio.Controllers
 {
@@ -11,7 +12,27 @@ namespace portfolio.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 17 ? "Dzień Dobry" : "Dobry Wieczór";
             return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
         }
     }
 }
