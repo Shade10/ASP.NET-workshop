a858d0657286178e5c9504b8b1a4b9718701c9b0
Marcin Kass <marcin.kass10@gmail.com>
2018-12-03 13:22:23 +0100

    added style for form invitation

--- portfolio/Models/GuestResponse.cs
+++ portfolio/Models/GuestResponse.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace AspWorkshop.Models
-{
-    public class GuestResponse
-    {
+namespace AspWorkshop.Models {
+    public class GuestResponse {
+        [Required(ErrorMessage = "Proszę podać imię i nazwisko")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę podać swój e-mail")]
+        [RegularExpression(".+\\@.+\\..+")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać swój telefon")]
         public string Phone { get; set; }
-        public bool WillAttend { get; set; }
+        [Required(ErrorMessage = "Proszę wybrać opcję")]
+        public bool? WillAttend { get; set; }
     }
 }
