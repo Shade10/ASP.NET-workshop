551639c271cbfca87ba16989c515a088d15ba7ee
Marcin Kass <marcin.kass10@gmail.com>
2018-12-03 12:24:16 +0100

    changed namespace and project name

--- portfolio/Controllers/HomeController.cs
+++ portfolio/Controllers/HomeController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using portfolio.Models;
+using AspWorkshop.Models;
 
-namespace portfolio.Controllers
+namespace AspWorkshop.Controllers
 {
     public class HomeController : Controller
     {
--- portfolio/Global.asax
+++ portfolio/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="portfolio.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspWorkshop.MvcApplication" Language="C#" %>
