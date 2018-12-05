01f4b79960815a9a453b0bbda6a47ac9fc1870a8
Marcin Kass <marcin.kass10@gmail.com>
2018-12-05 21:45:33 +0100

    added async method which get data from another page and return their lenght

---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LanguageFeatures.Models {
+    public class MyAsyncMethods {
+        public static Task<long?> GetPageLength() {
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://apress.com");
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+    }
+}
