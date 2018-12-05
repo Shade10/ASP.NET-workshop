3be20a547ede205b201486175ba3ff211009bac6
Marcin Kass <marcin.kass10@gmail.com>
2018-12-05 10:25:47 +0100

    testing some property on view

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public string Index() {
+            return "Jakiś text w indexie";
+        }
+
+        public ViewResult AutoProperty() {
+            Product myProduct = new Product();
+
+            myProduct.Name = "Kajak";
+            string productName = myProduct.Name;
+            return View("Index", (object) String.Format("Nazwa produktu: {0}", productName));
+        }
+    }
+}
