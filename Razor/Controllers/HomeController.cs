80377d05dfdab228bef31e94bccb2d17ff2e899a
Marcin Kass <marcin.kass10@gmail.com>
2018-12-06 12:27:23 +0100

    razor theme finished

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -28,6 +28,27 @@ namespace Razor.Controllers
         {
             return View(myProduct);
         }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Suplier = null;
+            return View(myProduct);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kajak", Price = 275M},
+                new Product {Name = "Kamizelka ratunkowa", Price = 48.95M},
+                new Product {Name = "Piłka nożna", Price = 19.50M},
+                new Product {Name = "Flaga narożna", Price = 34.95M}
+            };
+            return View(array);
+        }
     }
 
 }
