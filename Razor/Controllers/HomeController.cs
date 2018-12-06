41b58f9ece9d6c54836b7e786604b764898bf419
Marcin Kass <marcin.kass10@gmail.com>
2018-12-06 11:31:19 +0100

    added layout for basic view

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kajak",
+            Description = "Jednoosobowa łódka",
+            Category = "Sporty wodne",
+            Price = 275M
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+    }
+
+}
