a3cd4987d65300f4597ccd93ae8ee7f505d5200d
Marcin Kass <marcin.kass10@gmail.com>
2018-12-12 20:39:04 +0100

    added new project for tests and one class wchich will be tested

---
+++ EssentialTools.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialTools.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        private IDiscoutHelper getTestObject() {
+            return new MinimumDiscountHelper();
+        }
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            IDiscoutHelper target = getTestObject();
+            decimal total = 200;
+
+            var discountedTotal = target.ApplyDiscount(total);
+
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+    }
+}
---
+++ EssentialTools/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models {
+    public class MinimumDiscountHelper : IDiscoutHelper{
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
