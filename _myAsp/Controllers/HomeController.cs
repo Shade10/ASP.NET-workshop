a2495e67357d113c743b13e137e50e8d16ed6611
Marcin Kass <marcin.kass10@gmail.com>
2018-12-13 16:09:04 +0100

    prepared class for new form application for invitation guests

---
+++ _myAsp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _myAsp.Models;
+
+namespace _myAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Rsvp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Rsvp(GuestResponse guestResponse)
+        {
+            return View(string.Format("rsvp"));
+        }
+    }
+}
---
+++ _myAsp/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _myAsp.Models {
+    public class GuestResponse {
+        [Required(ErrorMessage = "Wprowadź swoje imię i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Wprowadź swój e-mail")]
+        [RegularExpression(".+\\@.+\\..+")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Wprowadź swój telefon")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Wybierz jedną z opcji")]
+        public bool? WillAttend { get; set; }
+
+
+    }
+}
