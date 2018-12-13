88ae7aaf77d9bd1444aa47396aeb182672a1f71c
Marcin Kass <marcin.kass10@gmail.com>
2018-12-13 11:49:36 +0100

    added new test for LinqValueCalculatore

---
+++ EssentialTools.Tests/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialTools.Tests {
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products =
+        {
+            new Product {Name = "Kajak", Category = "Sporty Wodne", Price = 275M},
+            new Product {Name = "Kamizelka ratunkowa", Category = "Sporty Wodne", Price = 48.95M},
+            new Product {Name = "Piłka nożna", Category = "Piłka nożna", Price = 19.50M},
+            new Product {Name = "Flaga narożna", Category = "Piłka nożna", Price = 34.95M}
+        };
+
+        [TestMethod]
+        public void Sum_Products_Correctly() {
+            var discounter = new MinimumDiscountHelper();
+            var target = new LinqValueCalculator(discounter);
+            var goalTotal = products.Sum(e => e.Price);
+
+            var result = target.ValueProducts(products);
+
+            Assert.AreEqual(goalTotal, result);
+        }
+    }
+}
