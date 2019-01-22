0bd9e8406df3675e2ebbe3954991ea3c42b3199a
Marcin Kass <marcin.kass10@gmail.com>
2019-01-22 12:13:55 +0100

    UaR - chenged default roouting

--- UrlAndRoutes/App_Start/RouteConfig.cs
+++ UrlAndRoutes/App_Start/RouteConfig.cs
@@ -11,13 +11,19 @@ namespace UrlAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            //Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            //routes.Add("MyRoute", myRoute);
+
+            //the same as abovex
+            routes.MapRoute("MyRoute", "{controller}/{action}");
         }
     }
 }
